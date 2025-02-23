127a9a0b773b588b685e604de769480d2a63a783
ardenos911 <ardenos@protonmail.com>
2025-02-23 04:59:47 -0500

    added some const changes to .env and index.ts

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "New Wave Vapes";
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "New Wave Vapes is a vape shop that sells state of the art vapes and vape accessories.";
-export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || "https://newwavevapes.com";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 export const APP_EMAIL = process.env.NEXT_PUBLIC_APP_EMAIL || "contact@newwavevapes.com";
 export const APP_PHONE = process.env.NEXT_PUBLIC_APP_PHONE || "+128877890";
 export const APP_ADDRESS = process.env.NEXT_PUBLIC_APP_ADDRESS || "17789 Atlantic Blvd, Jacksonville, FL, USA";
