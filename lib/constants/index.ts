3ed72d4d5a50d79da38b6ce7ba29b804c615c3e3
ardenos911 <ardenos@protonmail.com>
2025-02-23 04:41:12 -0500

    added AN .ENV FILE AND INDEX.TS FILE WITH MANY CONSTS

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,7 +1,7 @@
-export const APP_NAME = "New Wave Vapes";
-export const APP_DESCRIPTION = "New Wave Vapes is a vape shop that sells state of the art vapes and vape accessories.";
-export const APP_URL = "https://newwavevapes.com";
-export const APP_EMAIL = "contact@newwavevapes.com";
-export const APP_PHONE = "+128877890";
-export const APP_ADDRESS = "17789 Atlantic Blvd, Jacksonville, FL, USA";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "New Wave Vapes";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "New Wave Vapes is a vape shop that sells state of the art vapes and vape accessories.";
+export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || "https://newwavevapes.com";
+export const APP_EMAIL = process.env.NEXT_PUBLIC_APP_EMAIL || "contact@newwavevapes.com";
+export const APP_PHONE = process.env.NEXT_PUBLIC_APP_PHONE || "+128877890";
+export const APP_ADDRESS = process.env.NEXT_PUBLIC_APP_ADDRESS || "17789 Atlantic Blvd, Jacksonville, FL, USA";
 
