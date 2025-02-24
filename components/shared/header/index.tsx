912243fbea48a246da5c4f930b6fca37ab71c056
ardenos911 <ardenos@protonmail.com>
2025-02-24 03:03:42 -0500

    added new logo

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,7 +1,7 @@
-//import { ShoppingCart, UserIcon } from "lucide-react";
+import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-//import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
 
 
@@ -16,6 +16,18 @@ const Header = () => {
                         {APP_NAME}
                     </span>
                 </Link>
+            </div>
+            <div className="space-x-2">
+                <Button asChild variant="ghost">
+                    <Link href="/cart">
+                        <ShoppingCart className="w-5 h-5" /> Cart
+                    </Link>
+                </Button>
+                <Button asChild variant="ghost">
+                    <Link href="/sign-in">
+                        <UserIcon className="w-5 h-5" /> Sign In
+                    </Link>
+                </Button>
             </div>
         </div>
     </header>);
