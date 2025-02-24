15363513816062d55faa4c8ef4ed980c07c42a31
ardenos911 <ardenos@protonmail.com>
2025-02-24 02:02:27 -0500

    added images and changes to index.tsx

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,5 +1,20 @@
+import { ShoppingCart, UserIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+
+
 const Header = () => {
-    return ( <>Header</> );
+    return (<header className="w-full border-b">
+        <div className="wrapper flex-between">
+            <div className="flex-start">
+                <Link href="/" className="flex-start">
+                    <Image src="/images/logo.png" alt={APP_NAME} width={100} height={100} />
+                </Link>
+            </div>
+        </div>
+    </header>);
 }
- 
+
 export default Header;
